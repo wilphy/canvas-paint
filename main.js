293396d7a9ddff9055da858ca29457500b08af47
wilphy <149201216@qq.com>
2020-05-10 00:44:10 +0800

    优化

--- main.js
+++ main.js
@@ -1,63 +1,9 @@
 const canvas = document.getElementById("canvas");
-
-fullPage();
-
-window.onresize = () => fullPage();
-
-function fullPage() {
-  let pageWidth = document.documentElement.clientWidth;
-  let pageHeight = document.documentElement.clientHeight;
-
-  canvas.width = pageWidth;
-  canvas.height = pageHeight;
-}
-
 const ctx = canvas.getContext("2d");
 
-let using = false; // flag标记，是否开始画画
-let lastPoint = { x: undefined, y: undefined };
+autoSetCanvasSize(canvas);
 
-// 鼠标按下
-canvas.onmousedown = (a) => {
-  let x = a.clientX;
-  let y = a.clientY;
-  if (eraserEnabled) {
-    using = true;
-    ctx.clearRect(x, y, 10, 10);
-  } else {
-    using = true;
-    lastPoint = {
-      x: x,
-      y: y,
-    };
-  }
-};
-
-// 鼠标移动
-canvas.onmousemove = (a) => {
-  let x = a.clientX;
-  let y = a.clientY;
-
-  if (eraserEnabled) {
-    if (unescape) {
-      ctx.clearRect(x, y, 10, 10);
-    }
-  } else {
-    if (using) {
-      let newPonit = {
-        x: x,
-        y: y,
-      };
-      drawLine(lastPoint.x, lastPoint.y, newPonit.x, newPonit.y);
-      lastPoint = newPonit; // key
-    }
-  }
-};
-
-// 鼠标松开
-canvas.onmouseup = () => {
-  using = false;
-};
+listenToMouse(canvas);
 
 function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
@@ -71,5 +17,68 @@ function drawLine(x1, y1, x2, y2) {
 
 let eraserEnabled = false;
 eraser.onclick = () => {
-  eraserEnabled = !eraserEnabled;
+  eraserEnabled = true;
+  actions.className = "actions x";
 };
+
+panit.onclick = () => {
+  eraserEnabled = false;
+  actions.className = "actions";
+};
+
+// 设置画布大小
+function autoSetCanvasSize(canvas) {
+  setCanvasSize();
+
+  window.onresize = () => setCanvasSize();
+
+  function setCanvasSize() {
+    let pageWidth = document.documentElement.clientWidth;
+    let pageHeight = document.documentElement.clientHeight;
+
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+  }
+}
+
+// 监听鼠标事件
+function listenToMouse() {
+  let painting = false; // flag标记，是否开始画画
+  let lastPoint = { x: undefined, y: undefined };
+  // 鼠标按下
+  canvas.onmousedown = (a) => {
+    let x = a.clientX;
+    let y = a.clientY;
+    painting = true;
+    if (eraserEnabled) {
+      ctx.clearRect(x - 5, y - 5, 10, 10);
+    } else {
+      lastPoint = {
+        x: x,
+        y: y,
+      };
+    }
+  };
+
+  // 鼠标移动
+  canvas.onmousemove = (a) => {
+    let x = a.clientX;
+    let y = a.clientY;
+    if (!painting) return;
+    if (eraserEnabled) {
+      ctx.clearRect(x - 5, y - 5, 10, 10);
+    } else {
+      let newPonit = {
+        x: x,
+        y: y,
+      };
+      drawLine(lastPoint.x, lastPoint.y, newPonit.x, newPonit.y);
+      lastPoint = newPonit; // key
+    }
+  };
+
+  // 鼠标松开
+  canvas.onmouseup = () => {
+    painting = false;
+  };
+}
