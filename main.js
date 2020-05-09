3b8219a47f416df4e9b5b41a4034ae04770f68f6
wilphy <149201216@qq.com>
2020-05-10 00:00:10 +0800

    橡皮擦

--- main.js
+++ main.js
@@ -1,40 +1,63 @@
 const canvas = document.getElementById("canvas");
+
+fullPage();
+
+window.onresize = () => fullPage();
+
+function fullPage() {
+  let pageWidth = document.documentElement.clientWidth;
+  let pageHeight = document.documentElement.clientHeight;
+
+  canvas.width = pageWidth;
+  canvas.height = pageHeight;
+}
+
 const ctx = canvas.getContext("2d");
 
-let painting = false; // flag标记，是否开始画画
+let using = false; // flag标记，是否开始画画
 let lastPoint = { x: undefined, y: undefined };
 
 // 鼠标按下
 canvas.onmousedown = (a) => {
-  painting = true;
   let x = a.clientX;
   let y = a.clientY;
-  lastPoint = { x: x, y: y };
-  drawCircle(x, y, 1);
+  if (eraserEnabled) {
+    using = true;
+    ctx.clearRect(x, y, 10, 10);
+  } else {
+    using = true;
+    lastPoint = {
+      x: x,
+      y: y,
+    };
+  }
 };
 
 // 鼠标移动
 canvas.onmousemove = (a) => {
-  if (painting) {
-    let x = a.clientX;
-    let y = a.clientY;
-    let newPonit = { x: x, y: y };
-    drawCircle(x, y, 1);
-    drawLine(lastPoint.x, lastPoint.y, newPonit.x, newPonit.y);
-    lastPoint = newPonit; // key
+  let x = a.clientX;
+  let y = a.clientY;
+
+  if (eraserEnabled) {
+    if (unescape) {
+      ctx.clearRect(x, y, 10, 10);
+    }
+  } else {
+    if (using) {
+      let newPonit = {
+        x: x,
+        y: y,
+      };
+      drawLine(lastPoint.x, lastPoint.y, newPonit.x, newPonit.y);
+      lastPoint = newPonit; // key
+    }
   }
 };
 
 // 鼠标松开
 canvas.onmouseup = () => {
-  painting = false;
+  using = false;
 };
-
-function drawCircle(x, y, radius) {
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2);
-  ctx.fill();
-}
 
 function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
@@ -45,3 +68,8 @@ function drawLine(x1, y1, x2, y2) {
   ctx.stroke();
   ctx.closePath();
 }
+
+let eraserEnabled = false;
+eraser.onclick = () => {
+  eraserEnabled = !eraserEnabled;
+};
